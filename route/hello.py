435446a3e3a9f686ca657636c7959f78f9e6b201
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2025-07-30 12:53:55 +0900

    get params

--- route/hello.py
+++ route/hello.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from markupsafe import escape
 
 
 hello_bp = Blueprint("hello", __name__)
@@ -6,4 +7,10 @@ hello_bp = Blueprint("hello", __name__)
 
 @hello_bp.route("/hello", methods=["GET"])
 def hello():
-    return "hello"
+    name = request.args.get("name")
+    return f"hello, {name}"
+
+
+@hello_bp.route("/hello2/<name>", methods=["GET"])
+def hello2(name):
+    return f"hello, {escape(name)}"
