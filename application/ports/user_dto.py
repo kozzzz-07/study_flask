9656d597af24012cd8ebb7f23650872192044669
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2025-08-01 13:13:05 +0900

    DTOでバリデーション

--- application/ports/user_dto.py
+++ application/ports/user_dto.py
@@ -1,10 +1,10 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserCreateDTO(BaseModel):
-    name: str
-    age: int
+    name: str = Field(..., min_length=1)
+    age: int = Field(..., ge=0)
     nickname: Optional[str] = None
 
 
